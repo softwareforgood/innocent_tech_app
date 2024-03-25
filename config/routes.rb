c3c0b0218f713ccead452d1d2b01294cc0b0111c
Jason Vang <jvang@Jasons-MacBook-Air.local>
2024-03-25 13:18:52 -0500

    Created students controller,  index/show page, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'students', to: 'students#index'
+  get 'students/:id', to: 'students#show', as: 'student'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
