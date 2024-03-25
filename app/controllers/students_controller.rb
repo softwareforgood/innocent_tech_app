26044a8d8bcfc2b0dc9f399f2029105d29625853
Jcvang5 <143450521+Jcvang5@users.noreply.github.com>
2024-03-25 14:52:42 -0500

    Rubocop

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class StudentsController < ApplicationController
-#TO DO: Scope to single educator
+  # TO DO: Scope to single educator
   def index
     @students = Student.all
   end
