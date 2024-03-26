3a5e57300f11c30e42c9870219c404e932d33e68
Edith Emmings <edith@softwareforgood.com>
2024-03-26 13:13:21 -0500

    factory bot demo with student model

--- test/factories/classroom.rb
+++ test/factories/classroom.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
   factory :classroom do
     name { Faker::Lorem.word.titleize }
     association :school
+
+    trait :with_educator do
+      educators { create_list(:educator, 1, school:) }
+    end
   end
 end
