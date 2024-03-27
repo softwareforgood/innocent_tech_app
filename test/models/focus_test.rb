c4d831bb2ee1ab5bb348d0db5b7e642227d9de40
KNAthari <143450459+KNAthari@users.noreply.github.com>
2024-03-27 15:53:36 -0500

    Update focus_test.rb
    Added a new assertion to check if the focus had been saved successfully and flash a message saying so.

--- test/models/focus_test.rb
+++ test/models/focus_test.rb
@@ -7,6 +7,7 @@ class FocusTest < ActiveSupport::TestCase
   test 'should save new focus' do
     new_focus = create(:focus)
 
-    assert new_focus.save, 'New focus has not saved'
+    assert new_focus.save, 'new focus has not saved'
+    assert_not_nil new_focus.id, 'new focus has been saved succesfully'
   end
 end
