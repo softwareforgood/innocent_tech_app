6413bb8c1cd6adf9bd6688e99e22ce1003eb1227
Kenyon Nyangai <nyangaikenyon@gmail.com>
2024-03-27 12:48:51 -0500

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-load(Rails.root.join('db/seeds/classroom_setup.rb')) if Rails.env.development?
+load(Rails.root.join('db/seeds/classroom_setup.rb'),
+Rails.root.join('db/seeds/focus_setup.rb') 
+) if Rails.env.development?
+
+
 
 
