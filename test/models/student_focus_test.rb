919003e791a965f5fa60beba3c45cd04d4b8ae22
KNAthari <143450459+KNAthari@users.noreply.github.com>
2024-03-27 15:50:23 -0500

    Update student_focus_test.rb
    Added an assert to check and alert that the save was successful

--- test/models/student_focus_test.rb
+++ test/models/student_focus_test.rb
@@ -7,5 +7,6 @@ class StudentFocusTest < ActiveSupport::TestCase
   test 'Create Studentfocus' do
     studentfocus = build(:student_focus, active: true)
     assert studentfocus.save!, 'Student focus was not saved'
+    assert_not_nil studentfocus.id, 'Student focus was saved successfully'
   end
 end
