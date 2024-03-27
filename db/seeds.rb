657ed0ac9c964f3c7b5af2fbb4fbf710df762ba7
fouziah ahmed <fouziahahmed@fouziahs-MacBook-Air.local>
2024-03-27 00:47:47 -0500

    Created seed data for knowingCategory model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
 load(Rails.root.join('db/seeds/classroom_setup.rb')) if Rails.env.development?
+
+KnowingCategory.find_or_create_by(name: 'Interest', description: 'Interests/Passions', color: '#32b021', active: true)
+KnowingCategory.find_or_create_by(name: 'Talents', description: 'Talents/Skills', color: '#0c87cf', active: true)
+KnowingCategory.find_or_create_by(name: 'Dislikes', description: 'Dislikes/Challenges', color: 'ffba08', active: true)
+KnowingCategory.find_or_create_by(name: 'Family', description: 'Family/Home', color: '#642ca9', active: true)
+KnowingCategory.find_or_create_by(name: 'Friends', description: 'Friends/Peers', color: '#f75c03', active: true)
+KnowingCategory.find_or_create_by(name: 'Beliefs', description: 'Beliefs/Thoughts', color: '#d00000', active: true)
