50536e60e8f35efc0c43fd2429a7c29ab7173c30
fouziah ahmed <fouziahahmed@fouziahs-MacBook-Air.local>
2024-03-28 01:58:46 -0500

    Refactor: Update model StudentKnowing
    - Added validation to StudentKnowing model
    - Updated corresponding tests in the student_knowing_test.rb file

--- app/models/student_knowing.rb
+++ app/models/student_knowing.rb
@@ -4,4 +4,6 @@ class StudentKnowing < ApplicationRecord
   belongs_to :knowing_category
   belongs_to :student
   belongs_to :educator
+  validates :description, presence: true
+  validates :source_description, presence: true
 end
--- test/models/student_knowing_test.rb
+++ test/models/student_knowing_test.rb
@@ -6,4 +6,39 @@ class StudentKnowingTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should create a valid student knowing instance' do
+    student_knowing = FactoryBot.build(:student_knowing)
+    assert student_knowing.valid?
+  end
+
+  test 'should belong to a student' do
+    student_knowing = FactoryBot.build(:student_knowing, student: nil)
+    assert_not student_knowing.valid?
+  end
+
+  test 'should belong to an educator' do
+    student_knowing = FactoryBot.build(:student_knowing, educator: nil)
+    assert_not student_knowing.valid?
+  end
+
+  test 'should belong to a knowing category' do
+    student_knowing = FactoryBot.build(:student_knowing, knowing_category: nil)
+    assert_not student_knowing.valid?
+  end
+
+  test 'description should be present' do
+    student_knowing = FactoryBot.build(:student_knowing, description: nil)
+    assert_not student_knowing.valid?
+  end
+
+  test "source should be either 'primary' or 'secondary'" do
+    student_knowing = FactoryBot.build(:student_knowing)
+    assert_includes %w[primary secondary], student_knowing.source
+  end
+
+  test 'source_description should be present' do
+    student_knowing = FactoryBot.build(:student_knowing, source_description: nil)
+    assert_not student_knowing.valid?
+  end
 end
